bfecedebe9e3c46b848b9a0fe89d61922462de4d
Charles.H <100550614+fbcharles747@users.noreply.github.com>
2023-01-17 23:53:11 -0700

    input styling

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,5 +1,14 @@
+import { MdOutlineAttachFile } from "react-icons/md";
+import { BiImageAdd } from "react-icons/bi";
 export default function Input() {
-  return <div className="input">
-    Input
-  </div>;
+  return (
+    <div className="input">
+      <input type="text" placeHolder="Enter your message" />
+      <div className="send">
+        <BiImageAdd size={24} color="gray" />
+        <MdOutlineAttachFile size={24} color="gray" />
+        <button>Send</button>
+      </div>
+    </div>
+  );
 }
